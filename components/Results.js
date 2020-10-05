ed9b26cc64a420ce4f804aa6b851d16eb97e24ba
Shaurye Mahajan <43662406+shauryem@users.noreply.github.com>
2020-10-04 18:18:36 -0700

    Comments

--- components/Results.js
+++ components/Results.js
@@ -2,6 +2,8 @@ import BootstrapTable from "react-bootstrap-table-next";
 import paginationFactory from "react-bootstrap-table2-paginator";
 import { createPortal } from "react-dom";
 
+// Bootstrap tables used in all pages are structured in the following way
+
 function Results(props) {
   const data = props.data;
   const columns = props.columns;
