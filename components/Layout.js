e072ff11a77b002f1ac745b020e2e49744e2beb8
Shaurye Mahajan <43662406+shauryem@users.noreply.github.com>
2020-10-04 18:16:40 -0700

    Page structure comments

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,8 @@ import Container from "react-bootstrap/Container";
 import AppNavbar from "./AppNavbar";
 import AppFooter from "./AppFooter";
 
+// Every page is contrcuted in the same way: 
+// Top: NavBar | Middle: page | Bottom: Footer
 function Layout(props) {
   const user = props.user;
 
