430cbb5c31379e52229cd6dcc6ecaa5770e378a4
Shaurye Mahajan <43662406+shauryem@users.noreply.github.com>
2020-10-04 18:29:03 -0700

    Comment additions

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,5 +1,7 @@
 import auth0 from "../../utils/auth0";
 
+// Handles unauthorized access
+
 export default async function (req, res) {
   try {
     await auth0.handleCallback(req, res, { redirectTo: "/" });
